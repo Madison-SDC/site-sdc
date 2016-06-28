2fae0a3ff354c75e11d2879df7925a8801809869
vkottler <vaughnkottler@gmail.com>
2016-06-28 17:15:14 -0500

    Giving up on trying to add anything. Custom directive for header that's it, playing with dependencies.

---
+++ client/js/directives.js
@@ -0,0 +1,11 @@
+angular.module('directives', [])
+
+// Top Navbar
+.directive('topnav', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/topnav.html',
+    controller: 'mainController',
+    scope: true
+  }
+});
