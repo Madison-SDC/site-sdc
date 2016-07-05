7ce7f8dbc4cfe44586545079d0c31e58e12e0a94
EK <kreloff@wisc.edu>
2016-07-05 08:40:07 -0500

    Added scrolling for clicked links in navbar--I know the navbar looks like shit still fight me

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -2,3 +2,18 @@ angular.module('sdc')
 
 .controller("mainController", function($scope, $http) {
 });
+
+
+//Scrolling code below
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
