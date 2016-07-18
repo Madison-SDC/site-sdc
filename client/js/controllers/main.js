9996efbd96cd5c7a37f1c81bc9e58070595ae7bf
kreloff <kreloff@wisc.edu>
2016-07-18 16:35:56 -0500

    navbar in the works

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,22 +1,14 @@
 angular.module('sdc')
 
 .controller("mainController", function($scope, $http) {
-    $scope.welcome = "Angular test.";
 });
 
 
-//Scrolling code below
-// you can't do jquery and bootstrap
-/*
-$('a[href^="#"]').on('click', function(event) {
-
-    var target = $(this.getAttribute('href'));
-
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
+(function() {
+    'use strict';
+    angular.module('navBarDemoBasicUsage', ['ngMaterial'])
+        .controller('AppCtrl', AppCtrl);
+    function AppCtrl($scope) {
+        $scope.currentNavItem = 'page1';
     }
-});
-*/
+})();
